6b44444591cd7669227ae97ea1627a4a7ac81149
Sokolov Yura 'funny-falcon <funny.falcon@gmail.com>
2012-06-27 16:31:09 +0400

    reconnection for blocking socket, reconnect parameter for IProto.get_connection

--- lib/iproto.rb
+++ lib/iproto.rb
@@ -10,17 +10,17 @@ module IProto
   # types:
   # :em
   # :block
-  def self.get_connection(host, port, type = :block)
+  def self.get_connection(host, port, type = :block, reconnect = true)
     case type
     when :em
       require 'iproto/em'
-      ::EM.connect host, port, IProto::EM::FiberedConnection, host, port
+      ::EM.connect host, port, IProto::EM::FiberedConnection, host, port, reconnect
     when :em_callback
       require 'iproto/em'
-      ::EM.connect host, port, IProto::EM::CallbackConnection, host, port
+      ::EM.connect host, port, IProto::EM::CallbackConnection, host, port, reconnect
     when :block
       require 'iproto/tcp_socket'
-      IProto::TCPSocket.new(host, port)
+      IProto::TCPSocket.new(host, port, reconnect)
     else
       raise "Undefined type #{type}"
     end    
--- lib/iproto/tcp_socket.rb
+++ lib/iproto/tcp_socket.rb
@@ -1,29 +1,56 @@
 require 'socket'
 module IProto
   # TODO: timeouts
-  class TCPSocket < ::TCPSocket
+  class TCPSocket
     include IProto::ConnectionAPI
+    def initialize(host, port, reconnect = true)
+      @host = host
+      @port = port
+      @reconnect = true
+    end
+
+    def close
+      @reconnect = false
+      if @socket
+        @socket.close rescue nil
+      end
+    end
+
+    def socket
+      @socket ||= ::TCPSocket.new(@host, @port)
+    rescue Errno::ECONNREFUSED => e
+      raise CouldNotConnect, e
+    end
 
     # begin ConnectionAPI
     def send_request(request_type, body)
       request_id = next_request_id
-      r = send [request_type, body.bytesize, request_id].pack(PACK) + body, 0
+      r = socket.send [request_type, body.bytesize, request_id].pack(PACK) + body, 0
       response_size = recv_header request_id
       recv_response response_size
     rescue Errno::EPIPE => e
+      @socket = nil  if @reconnect
       raise Disconnected, e
     end
     # end ConnectionAPI
 
     def recv_header(request_id)
-      header = read(12)  or raise Disconnected, 'disconnected while read'
+      header = socket.read(12)  or begin
+        @socket = nil  if @reconnect
+        raise Disconnected, 'disconnected while read'
+      end
       type, response_size, recv_request_id = header.unpack(PACK)
-      raise UnexpectedResponse.new("Waiting response for request_id #{request_id}, but received for #{recv_request_id}") unless request_id == recv_request_id
+      unless request_id == recv_request_id
+        raise UnexpectedResponse.new("Waiting response for request_id #{request_id}, but received for #{recv_request_id}")
+      end
       response_size
     end
 
     def recv_response(response_size)
-      read(response_size)  or raise Disconnected, 'disconnected while read'
+      socket.read(response_size)  or begin
+        @socket = nil  if @reconnect
+        raise Disconnected, 'disconnected while read'
+      end
     end
   end
 end
