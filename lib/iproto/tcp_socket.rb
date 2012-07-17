0fa4ef096389302a4191b65766a7c091f32b2deb
Sokolov Yura 'funny-falcon <funny.falcon@gmail.com>
2012-07-17 18:35:43 +0400

    more sane tcp_socket reconnect

--- lib/iproto/tcp_socket.rb
+++ lib/iproto/tcp_socket.rb
@@ -4,12 +4,12 @@ module IProto
   class TCPSocket
     include IProto::ConnectionAPI
     def initialize(host, port, reconnect = true)
-      @host = host
-      @port = port
+      @addr = [host, port]
       @reconnect_timeout = Numeric === reconnect ? reconnect : DEFAULT_RECONNECT
       @reconnect = !!reconnect
       @socket = nil
       @reconnect_time = Time.now - 1
+      @retry = true
     end
 
     def close
@@ -26,33 +26,41 @@ module IProto
 
     def could_be_connected?
       @socket ? @socket != :disconnected
-              : @reconnect_time < Time.now
+              : (@retry || @reconnect_time < Time.now)
     end
 
     def socket
       if (sock = @socket)
         sock != :disconnected ? sock : raise(Disconnected, "disconnected earlier")
       else
-        @socket = ::TCPSocket.new(@host, @port)
+        sock = @socket = ::TCPSocket.new(*@addr)
+        @retry = true
       end
+      sock
     rescue Errno::ECONNREFUSED => e
       @socket = :disconnected  unless @reconnect
       raise CouldNotConnect, e
     end
 
+    class Retry < RuntimeError; end
+
     # begin ConnectionAPI
     def send_request(request_type, body)
-      request_id = next_request_id
-      r = socket.send ([request_type, body.bytesize, request_id].pack(PACK) << body), 0
-      response_size = recv_header request_id
-      recv_response response_size
-    rescue Errno::EPIPE => e
-      _raise_disconnected(e)
+      begin
+        request_id = next_request_id
+        r = socket.send ([request_type, body.bytesize, request_id].pack(PACK) << body), 0
+        response_size = recv_header request_id
+        recv_response response_size
+      rescue Errno::EPIPE, Retry => e
+        _raise_disconnected(e, !@retry)
+        @retry = false
+        retry
+      end
     end
     # end ConnectionAPI
 
     def recv_header(request_id)
-      header = socket.read(12)  or _raise_disconnected('disconnected while read')
+      header = socket.read(12)  or _raise_disconnected('disconnected while read', @retry ? :retry : true)
       type, response_size, recv_request_id = header.unpack(PACK)
       unless request_id == recv_request_id
         raise UnexpectedResponse.new("Waiting response for request_id #{request_id}, but received for #{recv_request_id}")
@@ -61,17 +69,22 @@ module IProto
     end
 
     def recv_response(response_size)
-      socket.read(response_size)  or _raise_disconnected('disconnected while read')
+      socket.read(response_size)  or _raise_disconnected('disconnected while read', 2)
     end
 
-    def _raise_disconnected(message)
+    def _raise_disconnected(message, _raise = true)
       if @reconnect
         @socket = nil
         @reconnect_time = Time.now + @reconnect_timeout
       else
         @socket = :disconnected
       end
-      raise Disconnected, message
+      case _raise
+      when true
+        raise Disconnected, message
+      when :retry
+        raise Retry
+      end
     end
   end
 end
